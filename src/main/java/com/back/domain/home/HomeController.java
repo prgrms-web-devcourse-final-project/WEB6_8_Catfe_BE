619a7b80ff64a1dcbffb29a1ca7223edaca0f2be
namgigun <namgigun@gmail.com>
2025-09-17 14:56:29 +0900

    Test: dev PR 승인 테스트

---
+++ src/main/java/com/back/domain/home/HomeController.java
@@ -0,0 +1,12 @@
+package com.back.domain.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
