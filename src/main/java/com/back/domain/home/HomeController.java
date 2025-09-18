49a4c3bae2984db3c74a09faccb5302a133e957e
namgigun <namgigun@gmail.com>
2025-09-18 21:24:43 +0900

    Infra: SpringDoc 설정 추가
    - localhost:8080 입력 시, 바로 redirect로 API 문서로 넘어감
    - SpringDoc 의존성 추가
    - SpringDoc 그룹화 설정 추가 -> API 주소가 /api/v1/** 인 것들 그룹화
    - 모든 API 응답의 기본 값 json으로 고정

--- src/main/java/com/back/domain/home/HomeController.java
+++ src/main/java/com/back/domain/home/HomeController.java
@@ -1,18 +1,17 @@
-package com.back.domain.test;
+package com.back.domain.home;
 
 import com.back.global.common.dto.RsData;
-import com.back.global.exception.CustomException;
 import com.back.global.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TestController {
-    @GetMapping("/")
-    public ResponseEntity<RsData<Void>> hello() {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(RsData.fail(ErrorCode.BAD_REQUEST));
+@Controller
+public class HomeController {
+    @GetMapping
+    public String main() {
+        return "redirect:/swagger-ui/index.html";
     }
 }
---
+++ src/main/java/com/back/global/config/SpringDocConfig.java
@@ -0,0 +1,21 @@
+package com.back.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Catfe API 서버", version = "beta", description = "Catfe API 문서입니다."))
+public class SpringDocConfig {
+
+    // API 버전별 그룹화
+    @Bean
+    public GroupedOpenApi groupApiV1() {
+        return GroupedOpenApi.builder()
+                .group("apiV1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+}
