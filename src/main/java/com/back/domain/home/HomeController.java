fdfc1c57ffbf13b7e35348c1158f202e55c2d08b
namgigun <namgigun@gmail.com>
2025-09-19 10:34:47 +0900

    Infra: SpringSecurity 설정 추가
    - build.gradle 시큐리티 의존성 추가
    - SecurityConfig 설정 (필터, CORS 설정, PasswordEncoder 빈 등록)

--- src/main/java/com/back/domain/home/HomeController.java
+++ src/main/java/com/back/domain/home/HomeController.java
@@ -6,12 +6,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping
     public String main() {
         return "redirect:/swagger-ui/index.html";
+    }
+
+    // CORS 스프링 시큐리티 필터 테스트용 컨트롤러 메서드
+    @GetMapping("/api/test")
+    @ResponseBody
+    public String test() {
+        return "Hello World";
     }
 }
---
+++ src/main/java/com/back/global/security/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.back.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(
+                        headers -> headers
+                                .frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                                )
+                )
+                .csrf(
+                        AbstractHttpConfigurer::disable
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:3000" // Next.js 개발 서버
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
