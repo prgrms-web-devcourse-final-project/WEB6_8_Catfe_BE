9a241fddec5c5ebc6ddf7b01d809efdc93f084e5
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-09-30 15:45:19 +0900

    Comment: 주석 추가

--- src/main/java/com/back/domain/user/dto/SendEmailRequest.java
+++ src/main/java/com/back/domain/user/dto/SendEmailRequest.java
@@ -3,6 +3,11 @@ package com.back.domain.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+/**
+ * 이메일 전송 요청을 나타내는 DTO
+ *
+ * @param email   이메일 주소
+ */
 public record SendEmailRequest(
         @NotBlank @Email String email
 ) {
