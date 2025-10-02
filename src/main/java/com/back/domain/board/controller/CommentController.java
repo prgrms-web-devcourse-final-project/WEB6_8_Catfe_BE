0a63cccdedc784140dbe12f818ff3d2af633ce1a
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-10-02 16:11:55 +0900

    Feat: 댓글 생성 API 구현 (#152) (#158)
    * Feat: 댓글 생성 API 구현
    
    * Test: 테스트 작성
    
    * Docs: Swagger 문서 작성

---
+++ src/main/java/com/back/domain/board/controller/CommentController.java
@@ -0,0 +1,36 @@
+package com.back.domain.board.controller;
+
+import com.back.domain.board.dto.CommentRequest;
+import com.back.domain.board.dto.CommentResponse;
+import com.back.domain.board.service.CommentService;
+import com.back.global.common.dto.RsData;
+import com.back.global.security.user.CustomUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts/{postId}/comments")
+@RequiredArgsConstructor
+public class CommentController implements CommentControllerDocs {
+    private final CommentService commentService;
+
+    // 댓글 생성
+    @PostMapping
+    public ResponseEntity<RsData<CommentResponse>> createComment(
+            @PathVariable Long postId,
+            @RequestBody @Valid CommentRequest request,
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        CommentResponse response = commentService.createComment(postId, request, user.getUserId());
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(RsData.success(
+                        "댓글이 생성되었습니다.",
+                        response
+                ));
+    }
+}
---
+++ src/main/java/com/back/domain/board/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.back.domain.board.repository;
+
+import com.back.domain.board.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
