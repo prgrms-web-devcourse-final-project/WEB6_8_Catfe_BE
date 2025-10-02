5c6666b86b825279ef446f681f30c39f484a77c7
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-10-02 12:34:05 +0900

    Feat: 게시글 수정 및 삭제 API 구현 (#131) (#151)
    * Feat: 게시글 수정 API 구현
    
    * FeaTest: 테스트 작성
    
    * Docs: Swagger 문서 작성
    
    * Feat: 게시글 삭제 API 구현
    
    * Test: 테스트 작성
    
    * Docs: Swagger 문서 작성

--- src/main/java/com/back/domain/board/controller/PostController.java
+++ src/main/java/com/back/domain/board/controller/PostController.java
@@ -65,4 +65,35 @@ public class PostController implements PostControllerDocs {
                         response
                 ));
     }
+
+    // 게시글 수정
+    @PutMapping("/{postId}")
+    public ResponseEntity<RsData<PostResponse>> updatePost(
+            @PathVariable Long postId,
+            @RequestBody @Valid PostRequest request,
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        PostResponse response = postService.updatePost(postId, request, user.getUserId());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(RsData.success(
+                        "게시글이 수정되었습니다.",
+                        response
+                ));
+    }
+
+    // 게시글 삭제
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<RsData<Void>> deletePost(
+            @PathVariable Long postId,
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        postService.deletePost(postId, user.getUserId());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(RsData.success(
+                        "게시글이 삭제되었습니다.",
+                        null
+                ));
+    }
 }
