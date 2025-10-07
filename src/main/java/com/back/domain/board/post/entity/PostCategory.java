6521ab41b442c1a0bd7547c3c4804b61c959dc37
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-10-07 13:58:18 +0900

    Feat: 카테고리 엔티티 수정 및 데이터 설정

--- src/main/java/com/back/domain/board/post/entity/PostCategory.java
+++ src/main/java/com/back/domain/board/post/entity/PostCategory.java
@@ -1,9 +1,8 @@
 package com.back.domain.board.post.entity;
 
+import com.back.domain.board.post.enums.CategoryType;
 import com.back.global.entity.BaseEntity;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +14,10 @@ import java.util.List;
 @Getter
 public class PostCategory extends BaseEntity {
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryType type;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PostCategoryMapping> postCategoryMappings;
