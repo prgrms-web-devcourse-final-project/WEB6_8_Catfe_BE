3d90a87143af2908115958ee78d44f835f0a3e8f
namgigun <namgigun@gmail.com>
2025-10-09 17:30:55 +0900

    Test: 파일 조회 기능 테스트 구현

--- src/test/java/com/back/domain/file/service/FileServiceTest.java
+++ src/test/java/com/back/domain/file/service/FileServiceTest.java
@@ -1,7 +1,9 @@
 package com.back.domain.file.service;
 
 import com.back.domain.board.post.entity.Post;
+import com.back.domain.board.post.repository.PostRepository;
 import com.back.domain.file.config.S3MockConfig;
+import com.back.domain.file.dto.FileReadResponseDto;
 import com.back.domain.file.dto.FileUploadResponseDto;
 import com.back.domain.file.entity.EntityType;
 import com.back.domain.user.entity.User;
@@ -9,6 +11,7 @@ import com.back.domain.user.entity.UserProfile;
 import com.back.domain.user.entity.UserStatus;
 import com.back.domain.user.repository.UserRepository;
 import io.findify.s3mock.S3Mock;
+import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -42,27 +45,58 @@ class FileServiceTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @AfterEach
     public void tearDown() {
         s3Mock.stop();
     }
 
     @Test
-    void upload() {
+    void uploadFile() {
         // given
-        String path = "test.png";
-        String contentType = "image/png";
-        String dirName = "test";
-
         User user = User.createUser("writer", "writer@example.com", passwordEncoder.encode("P@ssw0rd!"));
         user.setUserProfile(new UserProfile(user, "홍길동", null, "소개글", LocalDate.of(2000, 1, 1), 1000));
         user.setUserStatus(UserStatus.ACTIVE);
         userRepository.save(user);
 
+        Post post = new Post(user, "제목", "내용");
+        postRepository.save(post);
+
+        String path = "test.png";
+        String contentType = "image/png";
+        String dirName = "test";
+
         MockMultipartFile file = new MockMultipartFile("test", path, contentType, "test".getBytes());
 
         // when
-        FileUploadResponseDto res = fileService.uploadFile(file, EntityType.POST, 1L, 1L);
+        FileUploadResponseDto res = fileService.uploadFile(file, EntityType.POST, post.getId(), user.getId());
+
+        // then
+        assertThat(res.getImageUrl()).contains(path);
+        assertThat(res.getImageUrl()).contains(dirName);
+    }
+
+    @Test
+    void readFile() {
+        // given
+        User user = User.createUser("writer", "writer@example.com", passwordEncoder.encode("P@ssw0rd!"));
+        user.setUserProfile(new UserProfile(user, "홍길동", null, "소개글", LocalDate.of(2000, 1, 1), 1000));
+        user.setUserStatus(UserStatus.ACTIVE);
+        userRepository.save(user);
+
+        Post post = new Post(user, "제목", "내용");
+        postRepository.save(post);
+
+        String path = "test.png";
+        String contentType = "image/png";
+        String dirName = "test";
+        MockMultipartFile file = new MockMultipartFile("test", path, contentType, "test".getBytes());
+        fileService.uploadFile(file, EntityType.POST, post.getId(), user.getId());
+
+        //when
+        FileReadResponseDto res = fileService.getFile(EntityType.POST, post.getId());
 
         // then
         assertThat(res.getImageUrl()).contains(path);
