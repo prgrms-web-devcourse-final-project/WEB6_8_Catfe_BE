49c5dbe4acd9e9bc11c5d555c31c4a2e1941fb30
namgigun <namgigun@gmail.com>
2025-10-10 12:16:15 +0900

    Feat: 파일 업로드 유효성 검사 추가
    - EntityType, EntityId에 @NotBlank 적용

--- src/main/java/com/back/domain/file/dto/FileUpdateRequestDto.java
+++ src/main/java/com/back/domain/file/dto/FileUpdateRequestDto.java
@@ -1,12 +1,17 @@
 package com.back.domain.file.dto;
 
 import com.back.domain.file.entity.EntityType;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class FileUpdateRequestDto {
     private MultipartFile multipartFile;
+
+    @NotBlank(message = "entityType은 필수입니다.")
     private EntityType entityType;
+
+    @NotBlank(message = "entityId는 필수입니다.")
     private Long entityId;
 }
--- src/main/java/com/back/domain/file/dto/FileUploadRequestDto.java
+++ src/main/java/com/back/domain/file/dto/FileUploadRequestDto.java
@@ -1,6 +1,7 @@
 package com.back.domain.file.dto;
 
 import com.back.domain.file.entity.EntityType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +9,10 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 public class FileUploadRequestDto {
     private MultipartFile multipartFile;
+
+    @NotBlank(message = "entityType은 필수입니다.")
     private EntityType entityType;
+
+    @NotBlank(message = "entityId는 필수입니다.")
     private Long entityId;
 }
