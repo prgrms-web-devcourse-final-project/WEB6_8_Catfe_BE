3f681fd687422430cc1d8f29865642e1bee89ea0
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2025-10-11 18:05:44 +0900

    Ref: Comment Repository 개선

--- src/main/java/com/back/domain/board/comment/repository/custom/CommentLikeRepositoryImpl.java
+++ src/main/java/com/back/domain/board/comment/repository/custom/CommentLikeRepositoryImpl.java
@@ -4,17 +4,23 @@ import com.back.domain.board.comment.entity.QCommentLike;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
 
-
 @Repository
 @RequiredArgsConstructor
 public class CommentLikeRepositoryImpl implements CommentLikeRepositoryCustom {
-
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * 댓글 ID 목록 중 사용자가 좋아요한 댓글 ID 조회
+     * - 총 쿼리 수: 1회
+     *
+     * @param userId      사용자 ID
+     * @param commentIds  댓글 ID 목록
+     */
     @Override
     public List<Long> findLikedCommentIdsIn(Long userId, Collection<Long> commentIds) {
         QCommentLike commentLike = QCommentLike.commentLike;
