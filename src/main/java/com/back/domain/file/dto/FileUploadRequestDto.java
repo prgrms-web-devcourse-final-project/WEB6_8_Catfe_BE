bf3b625ce958e066f89fee6811d59fac4f3b77ee
namgigun <84756980+namgigun@users.noreply.github.com>
2025-10-12 16:11:27 +0900

    Feat: 파일 관련 기능 유효성 검사 추가 및 S3 버킷 정책 수정
    * Infra: 버킷 정책 추가
    
    - 이제 S3 오브젝트를 퍼블릭하게 읽을 수 있도록 허용
    
    * Feat: 파일 업로드 유효성 검사 추가
    
    - EntityType, EntityId에 @NotBlank 적용
    
    * Feat: EntityValidator 생성
    
    - EntityType, EntityId로 해당 데이터의 존재 유무를 체크
    
    * Refactor: 파일 서비스 리팩토링
    
    - 중복되는 로직 메서드화 -> 중복제거
    - entityType, entityId로 데이터가 존재하는지 체크하는 로직 추가
    
    * Feat: 유효성 검사 추가
    
    - 파일 업로드 시, 파일 입력을 필수로 지정
    
    * Feat: CORS 허용 URL 추가
    
    - Catfe 프론트 운영 서버 추가
    
    * Test: 파일 업로드 컨트롤러 테스트 코드 구현
    
    - 파일 업로드 성공한 경우
    - 파일 업로드 실패한 경우 - 업로드 시, 파일이 없는 경우
    
    * Fix: 파일 업로드 유효성 검사 수정
    
    - multiparFile 필드 만 @NotNull로 지정
    
    * Fix: Test 환경 S3Mock 수정
    
    - 기존 : withport(8001) -> 8001포트에 고정
    
    - 변경 : 할당 withport(0) -> 사용 가능한 랜덤포트 자동할당
    
    * Fix: 파일 컨트롤러 테스트 수정
    
    문제
    - 컨트롤러 테스트에서 S3Mock에 사용하는 포트를 점유하는 문제 발생 -> 서비스 테스트에서 S3Mock을 생성하지 못함
    
    해결
    - 컨트롤러 테스트가 끝나면 S3Mock의 포트를 해체 시켜 해당 문제 해결

--- src/main/java/com/back/domain/file/dto/FileUploadRequestDto.java
+++ src/main/java/com/back/domain/file/dto/FileUploadRequestDto.java
@@ -1,13 +1,17 @@
 package com.back.domain.file.dto;
 
 import com.back.domain.file.entity.EntityType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class FileUploadRequestDto {
+    @NotNull(message = "파일 입력은 필수입니다.")
     private MultipartFile multipartFile;
+
     private EntityType entityType;
+
     private Long entityId;
 }
