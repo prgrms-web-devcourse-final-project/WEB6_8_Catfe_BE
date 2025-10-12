4f16f352c26d38533ce876e19b2ca889b0904fe9
namgigun <namgigun@gmail.com>
2025-10-12 17:55:53 +0900

    Fix: 응답 데이터 수정
    - 기존: null
    - 변경: publicURL

--- src/main/java/com/back/domain/file/dto/FileUpdateResponseDto.java
+++ src/main/java/com/back/domain/file/dto/FileUpdateResponseDto.java
@@ -1,2 +1,12 @@
-package com.back.domain.file.dto;public class FileUpdateResponseDto {
+package com.back.domain.file.dto;
+
+import lombok.Data;
+
+@Data
+public class FileUpdateResponseDto {
+    private String publicURL;
+
+    public FileUpdateResponseDto(String publicURL) {
+        this.publicURL = publicURL;
+    }
 }
