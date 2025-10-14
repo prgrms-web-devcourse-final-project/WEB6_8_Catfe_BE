0da54e20445abb43ccf12594f59bf6d0e05500c2
namgigun <namgigun@gmail.com>
2025-10-14 14:51:37 +0900

    Fix: AttachmentMappingRepository 메서드 수정
    - findByEntityTypeAndEntityId -> findAllByEntityTypeAndEntityId

--- src/main/java/com/back/domain/file/repository/AttachmentMappingRepository.java
+++ src/main/java/com/back/domain/file/repository/AttachmentMappingRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AttachmentMappingRepository extends JpaRepository<AttachmentMapping, Long> {
-    List<AttachmentMapping> findByEntityTypeAndEntityId(EntityType entityType, Long entityID);
+    List<AttachmentMapping> findAllByEntityTypeAndEntityId(EntityType entityType, Long entityId);
 
     void deleteAllByEntityTypeAndEntityId(EntityType entityType, Long entityId);
 }
