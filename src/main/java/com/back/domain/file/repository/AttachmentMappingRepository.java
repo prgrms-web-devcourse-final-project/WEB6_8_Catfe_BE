4fbfb84aef52378d71960291694608694446a87c
namgigun <namgigun@gmail.com>
2025-10-14 12:46:39 +0900

    Feat: AttachmentMappingRepository 메서드 추가
    - deleteAllByEntityTypeAndEntityId (EntityType과 EnityId에 해당하는 파일 매핑 정보 모두 삭제)

--- src/main/java/com/back/domain/file/repository/AttachmentMappingRepository.java
+++ src/main/java/com/back/domain/file/repository/AttachmentMappingRepository.java
@@ -4,8 +4,11 @@ import com.back.domain.file.entity.AttachmentMapping;
 import com.back.domain.file.entity.EntityType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AttachmentMappingRepository extends JpaRepository<AttachmentMapping, Long> {
-    Optional<AttachmentMapping> findByEntityTypeAndEntityId(EntityType entityType, Long entityID);
+    List<AttachmentMapping> findByEntityTypeAndEntityId(EntityType entityType, Long entityID);
+
+    void deleteAllByEntityTypeAndEntityId(EntityType entityType, Long entityId);
 }
