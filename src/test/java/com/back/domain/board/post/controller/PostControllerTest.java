b70548c35af1ca819c6c919d4f872014ff050e12
loseminho <loseminho@gmail.com>
2025-10-15 11:39:56 +0900

    fix: mockbean 수정

--- src/test/java/com/back/domain/board/post/controller/PostControllerTest.java
+++ src/test/java/com/back/domain/board/post/controller/PostControllerTest.java
@@ -20,11 +20,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -66,7 +66,7 @@ class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private FileService fileService;
 
     private String generateAccessToken(User user) {
