56ff10a3281a097db3a78a9045ed0671f39f4f5d
namgigun <namgigun@gmail.com>
2025-10-30 15:50:11 +0900

    Infra: RDS 설정 변경
    - db 관련정보 하드 코딩 -> 변수화

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -455,9 +455,9 @@ resource "aws_db_instance" "mysql" {
   allocated_storage = 20
   storage_type      = "gp2"
 
-  db_name  = "catfe"
-  username = "catfe_user"
-  password = "catfe_pass"
+  db_name  = "${var.db_name}"
+  username = "${var.db_username}"
+  password = "${var.db_password}"
 
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg_1.id]
